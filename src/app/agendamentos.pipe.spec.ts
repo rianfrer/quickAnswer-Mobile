3b59c2575f5191ba26406b1f554ffd22348b9e5b
Rian Felipe Ferreira da Silva <rianfrer@gmail.com>
2023-04-11 08:52:54 -0300

    Foi feito melhorias no layout dos cards.

--- src/app/agendamentos.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AgendamentosPipe } from './agendamentos.pipe';
-
-describe('AgendamentosPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AgendamentosPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
